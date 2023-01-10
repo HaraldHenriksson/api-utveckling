6311ce1daaffe15d43de94075703a821c993d0fc
Harald <Harald@espinagar.se>
2023-01-10 14:00:25 +0100

    express server setup

---
+++ 02-express/server.js
@@ -0,0 +1,16 @@
+// EXPRESS SERVER
+
+
+const express = require('express')
+
+const app = express()
+
+const port = 3000
+
+app.get('/', (req, res) => {
+	req.send('Hello World')
+})
+
+app.listen(port, () => {
+	console.log(`Example listening on port ${port}`)
+})
