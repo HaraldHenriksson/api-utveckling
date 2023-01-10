440ef59a2563c61e92d8546ecfbdaa51dbc3db4a
Harald <Harald@espinagar.se>
2023-01-10 14:54:27 +0100

    respond with json

--- 02-express/server.js
+++ 02-express/server.js
@@ -8,11 +8,18 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-	res.send('Hello World🌏')
+	// res.send('Hello World 🌏')
+	res.send({
+		message: "Hello World 🌏",
+		lolcats: "are funny"})
+})
+
+app.post('/', (req, res) => {
+	res.send("I'm no mailbox 😡")
 })
 
 app.get('/coffee', (req, res) => {
-	res.send('Coffee☕️')
+	res.send('Coffee ☕️')
 })
 
 app.listen(port, () => {
