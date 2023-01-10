433ba40e51194c68ba8394c88df15297e19107a3
Harald <Harald@espinagar.se>
2023-01-10 11:11:06 +0100

    globals

--- 01-basics/01-globals.js
+++ 01-basics/01-globals.js
@@ -4,6 +4,9 @@ console.log('hello world')
 // 	cconsole.log("I'll be back")
 // }, 3000)
 
-setInterval( () => {
-	console.log("hello")
-}, 2000)
+// setInterval( () => {
+// 	console.log("hello")
+// }, 2000)
+
+console.log(__dirname)
+console.log(__filename)
