3f3d0d6f4b6c39afb93b8b33865c7326be3e1cbb
Harald <Harald@espinagar.se>
2023-01-10 12:07:13 +0100

    import and export. Modules

---
+++ 01-basics/02-calc.js
@@ -0,0 +1,11 @@
+
+// Calc
+
+const geometry = require('./modules/geomerty')
+
+
+console.log("Area of 2x4 box: ", geometry.boxArea(2, 4))
+
+console.log("Circ of 5x5 box: ", geometry.boxCircumference(5, 5))
+
+
---
+++ 01-basics/modules/geomerty.js
@@ -0,0 +1,13 @@
+// EVERYTHING GEOMETRY RELATED
+
+const boxArea = (w, h) => {
+	return w * h;
+}
+
+const boxCircumference = (w, h) => w * 2 + h *2;
+
+
+module.exports = {
+	boxArea, //  && boxArea: boxArea
+	boxCircumference,
+}
