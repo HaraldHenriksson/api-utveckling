aea46d62957754740994f45c5fea05106798f530
Harald <Harald@espinagar.se>
2023-01-12 11:44:09 +0100

    using morgan middleware

--- 02-express/server.js
+++ 02-express/server.js
@@ -5,19 +5,23 @@ const express = require('express')
 const oneliners = require('./data/oneliners.json')
 const _ = require('lodash')
 const app = express()
+const morgan = require('morgan')
 const port = 3000
 const now = new Date()
 
-// Log information about all incoming request. Middleware
-app.use( (req, res, next) => {
-	console.log("Someone requested something!")
-	// console.log(`Method: ${req.method}`)
-	// console.log(`Path: ${req.path}`)
-	console.log(`${now.toLocaleString()} Method: ${req.method} Path: ${req.path}`)
+// Middleware with morgan
+app.use( morgan('dev'))
 
-	// make code continue
-	next()
-})
+// Log information about all incoming request. Middleware
+// app.use( (req, res, next) => {
+// 	console.log("Someone requested something!")
+// 	// console.log(`Method: ${req.method}`)
+// 	// console.log(`Path: ${req.path}`)
+// 	console.log(`${now.toLocaleString()} Method: ${req.method} Path: ${req.path}`)
+
+// 	// make code continue
+// 	next()
+// })
 
 app.get('/', (req, res) => {
 	// res.send('Hello World 🌏')
@@ -59,11 +63,11 @@ app.get('/joke', (req, res) => {
 })
 
 // Catch requests where a route does not exist
-app.use( (req, res) => {
-	res.status(404).send({
-		message: `Sorry, no route exists for ${req.method} ${req.path}`
-	})
-})
+// app.use( (req, res) => {
+// 	res.status(404).send({
+// 		message: `Sorry, no route exists for ${req.method} ${req.path}`
+// 	})
+// })
 
 app.listen(port, () => {
 	console.log(`Example listening on localhost ${port}`)
