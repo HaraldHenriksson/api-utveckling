4514a8ed333ac5a14d232548f5e3e0534925db85
Harald <Harald@espinagar.se>
2023-01-12 09:41:48 +0100

    random GET request joke

--- 02-express/server.js
+++ 02-express/server.js
@@ -3,6 +3,8 @@
 
 const express = require('express')
 
+const oneliners = require('./data/oneliners.json')
+
 const app = express()
 
 const port = 3000
@@ -20,6 +22,17 @@ app.post('/', (req, res) => {
 
 app.get('/coffee', (req, res) => {
 	res.send('Coffee ☕️')
+})
+
+// GET random joke
+app.get('/joke', (req, res) => {
+
+	const i = Math.floor(Math.random() * oneliners.length)
+	const joke = oneliners[i]
+
+	res.send({
+		Joke: joke
+	})
 })
 
 app.listen(port, () => {
