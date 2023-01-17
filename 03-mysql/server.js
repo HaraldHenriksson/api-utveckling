b8adafd11e4e40f02ba3fb3f89922665c40c2678
Harald <Harald@espinagar.se>
2023-01-17 11:44:20 +0100

    server config with mamp working

--- 03-mysql/server.js
+++ 03-mysql/server.js
@@ -14,9 +14,10 @@ const mysql = require('mysql2')
 
 const connection = mysql.createConnection({
 	host: process.env.DATABASE_HOST,
+	port: process.env.DATABASE_PORT,
 	user: process.env.DATABASE_USER,
 	password: process.env.DATABASE_PASSWORD,
-	name: process.env.DATABASE_NAME,
+	database: process.env.DATABASE_NAME,
 })
 
 // Create a new Express app
