61c0712a5294e0b70d57e9621bc87c1d49ddc5d2
Harald <Harald@espinagar.se>
2023-01-24 10:29:08 +0100

    morgan and express dependencies

--- 04-prisma/src/app.ts
+++ 04-prisma/src/app.ts
@@ -9,10 +9,34 @@ app.use(morgan('dev'))
 /**
  * GET /
  */
-app.get('/', (req, res) => {
-	res.send({
-		message: "I AM API, BEEP BOOP",
-	})
+app.get('/users', async (req, res) => {
+	try {
+	const users = await prisma.users.findMany()
+	res.send(users)
+	} catch (err) {
+		console.error(err)
+		res.status(500).send( { message: "Something went wrong quering the database."})
+	}
+})
+
+app.get('/phones', async (req, res) => {
+	try {
+	const phones = await prisma.users.findMany()
+	res.send(phones)
+	} catch (err) {
+		console.error(err)
+		res.status(500).send( { message: "Something went wrong quering the database."})
+	}
+})
+
+app.get('/phones', async (req, res) => {
+	try {
+	const phones = await prisma.users.findUnique()
+	res.send(phones)
+	} catch (err) {
+		console.error(err)
+		res.status(500).send( { message: "Something went wrong quering the database."})
+	}
 })
 
 export default app
