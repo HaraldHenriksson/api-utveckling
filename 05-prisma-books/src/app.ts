4e7925fa5f9842beac509a286a61ef5fd29bca0b
Harald <Harald@espinagar.se>
2023-01-26 12:03:40 +0100

    get and post

--- 05-prisma-books/src/app.ts
+++ 05-prisma-books/src/app.ts
@@ -111,4 +111,52 @@ app.post('/books', async (req, res) => {
 	}
 })
 
+
+// GET / PUBLISHERS
+
+app.get('/publishers', async (req, res) => {
+	try {
+		const publisher = await prisma.publisher.findMany()
+		res.send(publisher)
+	} catch (err) {
+		res.status(500).send({ message: "Something went wrong"})
+	}
+})
+
+// GET / PPUBLUSHER / PUBLISHERID
+app.get('/publisher/:publisherId', async (req, res) => {
+	const publisherId = Number(req.params.publisherId)
+	try {
+		const publisher = await prisma.publisher.findUniqueOrThrow({
+			where: {
+				id: publisherId,
+			},
+			include: {
+				books: true,
+			}
+		})
+		res.send(publisher)
+	} catch (err) {
+		res.status(404).send({ message: "Not found"})
+	}
+})
+
+
+// POST / publishers
+
+app.post('/publishers', async (req, res) => {
+	try {
+		const publisher = await prisma.publisher.create({
+			data: {
+				name: req.body.name,
+			}
+		})
+
+		res.status(201).send(publisher)
+
+	} catch (err) {
+		res.status(500).send({ message: "Something went wrong"})
+	}
+})
+
 export default app
