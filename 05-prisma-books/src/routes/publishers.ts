823b5329044035db5dbafccac244f8827cd975a7
Harald <Harald@espinagar.se>
2023-01-26 16:23:24 +0100

    publisher controller added

--- 05-prisma-books/src/routes/publishers.ts
+++ 05-prisma-books/src/routes/publishers.ts
@@ -1,53 +1,20 @@
 import express from 'express'
-import prisma from '../prisma'
+import { index, show, store } from '../controllers/publisher_controller'
 const router = express.Router()
 
-// GET / PUBLISHERS
+/**
+ * GET /publishers
+ */
+router.get('/', index)
 
-router.get('/', async (req, res) => {
-	try {
-		const publisher = await prisma.publisher.findMany()
-		res.send(publisher)
-	} catch (err) {
-		res.status(500).send({ message: "Something went wrong"})
-	}
-})
+/**
+ * GET /publishers/:publisherId
+ */
+router.get('/:publisherId', show)
 
-// GET / PPUBLUSHER / PUBLISHERID
-router.get('/:publisherId', async (req, res) => {
-	const publisherId = Number(req.params.publisherId)
-	try {
-		const publisher = await prisma.publisher.findUniqueOrThrow({
-			where: {
-				id: publisherId,
-			},
-			include: {
-				books: true,
-			}
-		})
-		res.send(publisher)
-	} catch (err) {
-		res.status(404).send({ message: "Not found"})
-	}
-})
-
-
-// POST / publishers
-
-router.post('/', async (req, res) => {
-	try {
-		const publisher = await prisma.publisher.create({
-			data: {
-				name: req.body.name,
-			}
-		})
-
-		res.status(201).send(publisher)
-
-	} catch (err) {
-		res.status(500).send({ message: "Something went wrong"})
-	}
-})
-
+/**
+ * POST /publishers
+ */
+router.post('/', store)
 
 export default router
