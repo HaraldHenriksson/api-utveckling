4b9be2ed2def88d35a14ce1e51e37d3c3d4b2893
Harald <Harald@espinagar.se>
2023-01-31 14:38:34 +0100

    register controller and validation

---
+++ 05-prisma-books/src/controllers/register_controller.ts
@@ -0,0 +1,30 @@
+/**
+ * Register Controller
+ */
+import { Request, Response } from 'express'
+import { matchedData, validationResult } from 'express-validator'
+import prisma from '../prisma'
+
+/**
+ * Register a new user
+ */
+export const register = async (req: Request, res: Response) => {
+	// Validate incoming data
+	const validationErrors = validationResult(req)
+	if (!validationErrors.isEmpty()) {
+		return res.status(400).send({
+			status: "fail",
+			data: validationErrors.array(),
+		})
+	}
+
+	// Get only the validated data from the request
+	const validatedData = matchedData(req)
+
+	// Calculate a hash + salt for the password
+
+	// store the user in the database
+
+	// Resopns with 201 Created + status success
+	res.status(201).send({ "status": "success", "data": validatedData })
+}
--- 05-prisma-books/src/routes/index.ts
+++ 05-prisma-books/src/routes/index.ts
@@ -1,7 +1,9 @@
 import express from "express"
+import { body } from 'express-validator'
 import authors from './authors'
 import books from './books'
 import publishers from './publishers'
+import { register } from '../controllers/register_controller'
 
 // instantiate a new router
 const router = express.Router()
@@ -29,5 +31,23 @@ router.use('/books', books)
  * /publishers
  */
 router.use('/publishers', publishers)
+
+/**
+ * /register
+ */
+router.post('/register',[
+	// place validation rules here
+
+	// name required + at least 3 chars
+	body('name').isString().withMessage('has to be a string').bail().isLength({ min: 3, max: 191 }).withMessage('has to be 3-191 chars long'),
+
+	// email required + valid email
+	body('email').isEmail(),
+
+	// password required
+	body('password').isString().bail().isLength({ min: 6 }),
+
+], register)
+
 
 export default router
