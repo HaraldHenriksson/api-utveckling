0dd25c1b13963f115a63a759c907b5037c65f89b
Harald <Harald@espinagar.se>
2023-02-07 09:24:36 +0100

    getProfile

--- 05-prisma-books/src/controllers/profile_controller.ts
+++ 05-prisma-books/src/controllers/profile_controller.ts
@@ -1,16 +1,21 @@
 /**
  * Profile Controller
  */
+import Debug from 'debug'
 import { Request, Response } from 'express'
 import prisma from '../prisma'
+const debug = Debug("prisma-books:profile_controller")
 
 /**
  * Get the authenticated user's profile
  */
 export const getProfile = async (req: Request, res: Response) => {
+
+	debug("WHO DIS??", req.user)
 	res.send({
-		status: "success",
-		data: null,
+		id: req.user.id,
+		name: req.user.name,
+		email: req.user.email,
 	})
 }
 
