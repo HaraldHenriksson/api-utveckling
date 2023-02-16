eb77dd4114a8c151e00025d306587d5f3c5b581a
Harald <Harald@espinagar.se>
2023-02-16 10:26:25 +0100

    schema

--- 06-lmdb/src/resources/movie/movie.controller.ts
+++ 06-lmdb/src/resources/movie/movie.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express'
 import Debug from 'debug'
 import { Movie } from './movie.model'
+import mongoose from 'mongoose'
 
 const debug = Debug('lmdb:movie.controller')
 
@@ -30,15 +31,17 @@ export const index = async (req: Request, res: Response) => {
  */
 export const show = async (req: Request, res: Response) => {
 	const movieId = req.params.movieId
+
 	try {
 		// Find a single movie
-		const movie = await Movie.find()
+		const movie = await Movie.findById(movieId)
 
 		// If no movie was found, report 404
 		if (!movie) {
 			return res.sendStatus(404)
 		}
 
+		// Respond with movie
 		res.send({
 			status: "success",
 			data: movie,
@@ -57,17 +60,24 @@ export const show = async (req: Request, res: Response) => {
  */
 export const store = async (req: Request, res: Response) => {
 	try {
-		// Create a new movie
+		// Create and save a new Movie
 		const movie = await new Movie(req.body).save()
 
-		// Respond with newly created movie
+		// Respond with the newly created Movie
 		res.status(201).send({
 			status: "success",
 			data: movie,
 		})
 
+		const err = new Error()
+
 	} catch (err) {
 		debug("Error thrown when creating movie", err)
-		res.status(500).send({ status: "error", message: "Error thrown when finding movies" })
+
+		if (err instanceof mongoose.Error.ValidationError) {
+			return res.status(400).send({ status: "error", message: err.message })
+		}
+
+		res.status(500).send({ status: "error", message: "Error thrown when creating a new movie" })
 	}
 }
--- 06-lmdb/src/resources/movie/movie.model.ts
+++ 06-lmdb/src/resources/movie/movie.model.ts
@@ -4,12 +4,32 @@ export interface IMovie extends Document {
 	title: string,
 	runtime?: number,
 	releaseYear?: number,
+	genre?: string,
 }
 
 const MovieSchema: Schema = new Schema<IMovie>({
-	title: { type: String, required: true },
-	runtime: Number,
-	releaseYear: Number,
+	title: {
+		type: String,
+		required: true,
+		trim: true,
+		minlength: 3,
+		unique: true,
+	},
+	runtime: {
+		type: Number,
+		 min: 1
+	},
+	releaseYear: {
+		 type: Number,
+		 min: 1888,
+		 max: new Date().getFullYear(),
+		 default: new Date().getFullYear(),
+	},
+	genre: {
+		type: String,
+		lowercase: true,
+		enum: ["action", "sci-fi", "bromance"],
+	},
 })
 
 export const Movie = model<IMovie>('Movie', MovieSchema)
