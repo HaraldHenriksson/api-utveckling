7a853035f4689a106f5e39f635bd2ad359d9933d
Harald <Harald@espinagar.se>
2023-02-21 11:45:18 +0100

    moved handling of cliend connectio n to its own controller

--- 07-chat/backend/server.ts
+++ 07-chat/backend/server.ts
@@ -2,6 +2,7 @@ import app from './src/app'
 import http from 'http'
 import * as dotenv from 'dotenv'
 import { Server } from 'socket.io'
+import { handleConnection } from './src/controllers/socket_controller'
 
 // Initialize dotenv so it reads our `.env`-file
 dotenv.config()
@@ -24,7 +25,7 @@ const io = new Server(httpServer, {
  * Handle incoming Socket.Io connection
  */
 io.on('connection', (socket) => {
-	console.log('Yay someone connected!🥳', socket.id)
+	handleConnection(socket)
 })
 
 /**
--- 07-chat/backend/src/controllers/socket_controller.ts
+++ 07-chat/backend/src/controllers/socket_controller.ts
@@ -2,6 +2,17 @@
  * Socket Controller
  */
 import Debug from 'debug'
+import { Socket } from 'socket.io'
 
 // Create a new debug instance
 const debug = Debug('chat:socket_controller')
+
+// Handle the user socket connection
+export const handleConnection = (socket: Socket) => {
+	debug('👋 A user connected', socket.id)
+
+	// Handle user disconnecting
+	socket.on('disconnect', () => {
+		debug('🚶 A user disconnected', socket.id)
+	})
+}
